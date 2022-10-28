ddf27f25c359ad146f8bbf66b110dc8cbfb96798
rageenagit <116886978+rageenagit@users.noreply.github.com>
2022-10-28 17:09:46 +0530

    Create balance.java

---
+++ balance.java
@@ -0,0 +1,18 @@
+ code  
+public static void main(String[] args)  
+{  
+//calling the user-defined method inside the if statement and print the result accordingly    
+if (isKeith(19))  
+System.out.println("Yes, the given number is a Keith number.");  
+else  
+System.out.println("No, the given number is not a Keith number.");  
+if(isKeith(742))  
+System.out.println("Yes, the given number is a Keith number.");  
+else  
+System.out.println("No, the given number is not a Keith number.");  
+if(isKeith(4578))  
+System.out.println("Yes, the given number is a Keith number.");  
+else  
+System.out.println("No, the given number is not a Keith number.");  
+}  
+}  
